8a72190544f8315ecb93844e646c240557345228
Tomas Leriche <samotleriche@gmail.com>
2020-03-01 22:14:30 -0600

    added word model

---
+++ models/Word.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+
+const WordSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a course title']
+    },
+    definition: {
+        type: String,
+        required: [true, 'Please add a definition']
+    },
+    POS: {
+        type: String,
+        required: [true, 'Please add a part of speach'],
+        enum: ['noun', 'verb', 'adjective', 'adverb']
+    },
+    origin: {
+        type: String,
+        
+    },
+    pronouce: {
+        type: String,
+    },
+    progress: {
+        type: String,
+        required: [true, 'Please add a progress amount'],
+        enum: ['none', 'learning', 'learned']
+    },
+    quiz: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Quiz',
+        required: true
+    }
+
+});
+
+module.exports = mongoose.model('Word', WordSchema)
