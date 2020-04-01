04a88a17ce683b1b4d704741484d71b34dd9a0c5
Tomas Leriche <samotleriche@gmail.com>
2020-03-31 22:16:30 -0700

    began adding protected routes

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,34 +1,42 @@
-const express = require('express');
-const { 
-    getQuiz, 
-    getQuizzes, 
-    updateQuiz, 
-    createQuiz, 
-    deleteQuiz
-} = require('../controllers/quizzes');
+const express = require("express");
+const {
+  getQuiz,
+  getQuizzes,
+  updateQuiz,
+  createQuiz,
+  deleteQuiz
+} = require("../controllers/quizzes");
 
-const Quiz = require('../models/Quiz');
-const advancedResults = require('../middleware/advancedResults');
+const Quiz = require("../models/Quiz");
+const advancedResults = require("../middleware/advancedResults");
 
 const router = express.Router();
 
+const { protect } = require("../middleware/auth");
+
 // Include other resource routers
 
-const wordRouter = require('./words');
+const wordRouter = require("./words");
 
 // Re-route into other resource routers
-router.use('/:quizId/words', wordRouter);
+router.use("/:quizId/words", wordRouter);
 
-router.route('/')
-    .get(advancedResults(Quiz, {
-            path: "words",
-            select: "title POS"
-          }), getQuizzes)
-    .post(createQuiz);
+router
+  .route("/")
+  .get(
+    protect,
+    advancedResults(Quiz, {
+      path: "words",
+      select: "title POS"
+    }),
+    getQuizzes
+  )
+  .post(createQuiz);
 
-router.route('/:id')
-    .get(getQuiz)
-    .put(updateQuiz)
-    .delete(deleteQuiz);
+router
+  .route("/:id")
+  .get(getQuiz)
+  .put(updateQuiz)
+  .delete(deleteQuiz);
 
 module.exports = router;
